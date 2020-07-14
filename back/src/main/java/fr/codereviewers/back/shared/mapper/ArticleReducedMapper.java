f2b8567e730a2adb558af65bf4403ec62c61986f
xersos <corentin.jacob@uha.fr>
2020-07-14 22:30:27 +0200

    feat: config docker

--- back/src/main/java/fr/codereviewers/back/shared/mapper/ArticleReducedMapper.java
+++ back/src/main/java/fr/codereviewers/back/shared/mapper/ArticleReducedMapper.java
@@ -2,6 +2,8 @@ package fr.codereviewers.back.shared.mapper;
 
 import fr.codereviewers.back.core.entity.ArticleEntity;
 import fr.codereviewers.back.web.dto.ArticleInfoReducedDto;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface ArticleReducedMapper extends IGenericMapper<ArticleEntity, ArticleInfoReducedDto> {
 }
